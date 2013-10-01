75787d48eb1275b84e12b27d4866e741e7ccb6de
KD-Chang <ibelieveicaner@gmail.com>
2013-10-01 10:48:54 +0800

    fb like 1001

--- sample/whatsfood/js/index.js
+++ sample/whatsfood/js/index.js
@@ -56,7 +56,7 @@ var getDetail = function(){
                //      var recommendation = data.query.results.body.div.div[6].table.tr[9].td.p;
                // else
                //      var recommendation = '無資料';
-            $('#dining-content').html('<dvi>' + '<h3>' + shop + '</h3><br>' + '<h4>營業時間：</h4>' + '<p>' + time + '</p>' + '<h4>商家地址：</h4>' + '<p>' + address + '</p>' + '<a href="tel:' + tel + '"><button class="success" type="submit">電話訂位</button></a>' + '</div>');
+            $('#dining-content').html('<dvi>' + '<h3>' + shop + '</h3>' + '<h4>營業時間：</h4>' + '<p>' + time + '</p>' + '<h4>商家地址：</h4>' + '<p>' + address + '</p>' + '<a href="tel:' + tel + '"><button class="success" type="submit">電話訂位</button></a>' + '</div>');
         });
     });
 };
